2d492e8e1dd753b713c3dd04781afcf4d7ed7c7c
NonExstnt <64533120+NonExstnt@users.noreply.github.com>
2025-05-13 18:07:00 +1000

    Ideal solution from boot.dev

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
-from stats import get_num_words, get_char_count, sort_by_count
 import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
-def get_book_text(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-    return file_contents
 
-def format(file_path, num_words, char_num):
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {file_path}...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    for i in range(0, len(char_num)):
-        print(f"{char_num[i]["char"]}: {char_num[i]["num"]}")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
     print("============= END ===============")
 
-def arg_check():
-    if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
-    return sys.argv[1]
 
-def main():
-    file_path = arg_check()
-    frankenstein_text = get_book_text(file_path)
-    num_words = get_num_words(frankenstein_text)
-    char_count = get_char_count(frankenstein_text)
-    sorted_char = sort_by_count(char_count)
-    format(file_path, num_words, sorted_char)
 main()

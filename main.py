ee37ec1c5a921299957875758ba34ce6552cf9bb
NonExstnt <64533120+NonExstnt@users.noreply.github.com>
2025-05-13 16:48:51 +1000

    Edits to main.py & stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import get_num_words
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -6,7 +6,7 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
 
 main()

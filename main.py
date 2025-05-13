a3f30b90ac3182b869708fa07b6c844eb7d35cd9
NonExstnt <64533120+NonExstnt@users.noreply.github.com>
2025-05-13 16:40:21 +1000

    updated main.py reads file and counts words

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    print(f"{num_words} words found in the document")
+
+main()
